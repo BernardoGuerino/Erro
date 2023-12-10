fff880e5c1ffebab8c8ac45136d49608e21f2dd1
BernardoGuerino <bernardo.coelho.guerino@gmail.com>
2023-12-10 14:02:25 -0300

    .

--- Agenda/routes/web.php
+++ Agenda/routes/web.php
@@ -14,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('cadastro');
-});
+Route::get('/', function () {return view('cadastro');});
 
-Route::get('login', function () {
-    return view('login');
-});
+Route::get('login', function () {return view('login');});
 
-Route::get('agenda', function () {
-    return view('agenda');
-});
+Route::get('agenda', function () {return view('agenda');});
